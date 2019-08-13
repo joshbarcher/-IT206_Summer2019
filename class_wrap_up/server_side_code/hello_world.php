63ab9ee1b55d965136a977a072fa8d2503e65511
Josh Archer <jarcher@greenriver.edu>
2019-08-13 12:40:24 -0700

    Added a last few examples of writing server-side code with PHP and client-side code with Javascript.

---
+++ class_wrap_up/server_side_code/hello_world.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dynamic Server Content</title>
+</head>
+<body>
+    <h1><?php echo "hello world!"; ?></h1>
+    <p>Today is <?php echo date("h:i:s", time()); ?></p>
+</body>
+</html>
